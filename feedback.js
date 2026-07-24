95bb2bcf760fcd8cb46d9ece50c55a588ff9321c
TomhollandZendaya-34 <selvabharathi942@gmail.com>
2026-07-24 20:11:46 +0530

    Implement feedback form submission with emailjs

---
+++ feedback.js
@@ -0,0 +1,78 @@
+const btn = document.getElementById("sendBtn");
+const box = document.getElementById("message");
+const success = document.getElementById("success");
+const quoteOverlay = document.getElementById("quoteOverlay");
+
+btn.addEventListener("click", () => {
+
+    const message = box.value.trim();
+
+    if(message===""){
+
+        alert("Please type your thoughts ❤️");
+
+        return;
+
+    }
+
+    emailjs.send(
+
+        "service_1kflh76",
+
+        "template_ya0ui7v",
+
+        {
+
+            message:message
+
+        }
+
+    )
+
+    .then(()=>{
+
+       confetti({
+        particleCount:250,
+        spread:180,
+        origin:{y:.6}
+    });
+
+    success.style.display="block";
+
+    box.value="";
+
+    // Success message 2.5 seconds
+    setTimeout(()=>{
+
+        success.style.display="none";
+
+        // Show quote
+quoteOverlay.classList.add("show");
+
+// Restart signature animation
+const signature = document.querySelector(".signature");
+
+signature.classList.remove("animate");
+void signature.offsetWidth; // Force reflow
+signature.classList.add("animate");
+
+// Hide quote after 20 seconds
+setTimeout(()=>{
+
+    quoteOverlay.classList.remove("show");
+
+},20000);
+
+    },2500);
+
+})
+
+    .catch((error)=>{
+
+        console.log(error);
+
+        alert("Message could not be sent.");
+
+    });
+
+});
