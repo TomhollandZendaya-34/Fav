47fa2738747ab111edd9238bcbc0646d69390602
TomhollandZendaya-34 <selvabharathi942@gmail.com>
2026-07-24 21:45:53 +0530

    Update calendarData.js

--- calendarData.js
+++ calendarData.js
@@ -3,22 +3,22 @@ const calendarData = {
     year: 2026,
     month: 1,
     memories: {
-      8: ["📍Tiruppalai la un Frnd Harinee oda Mehandi Function ku unna drop pannen aprm Meenakshi amman temple la irunthu unna pickup pannitu nee church paathadhu illa nu sonna unna church kootitu ponen first time nee church vantha aprm Yum Yards la chicken loaded fries saaptutu clg poitom"],
-      10: ["🏍 Scooty Service eduthutu aprm Mug & Beans poitu clg vanthu scooty ya parking la vitutu clg velila iruntha waffles kadaila waffles saaptom"],
-      11: ["🚶Clg la irunthu Theppakulam vara first time traffic la scooty a drive panna pudhu scooty vaangi first time long journey for you enakum unkooda scooty la vanthathu adhaan first time aprm unaku pudicha 90's mittai theppakulam la vaangunom"],
-      12: ["⛰ Appa Amma Akka kooda barbeque nation porathuku unna thiruparangundram la drop pannitu vanthen"],
-      13: ["🍽 Namma Kadai la night dinner saaptom"],
-      14: ["🌄 Enna First time Samanar Hills kottitu pona anga poitu photo eduthutu aprm gowri sankar hotel la saaptutu trends poitu unaku dress vaangunom"],
-      16: ["🥣 Thirunagar la Mini Idly cravings a fulfill pannom"]
-      17: []
-      18: []
-      19: []
-      20: []
-      23: []
-      24: []
-      25: []
-      27: []
-      29: []
+      8: ["This was the day I was sitting in a barber shop and asked your number to thejal and that was the time I called you and spoke about LSSBE"],
+      10: ["I texted you will you to attend LSSBE class and you asked me to reserve a seat near me cause you don't know anyone "],
+      11: ["Our first LSSBE class together .You gpayed me for buying you a juice in cafe and it all started that day. And at afternoon you were complementing me and I was acting so nonchalant that day 😬(TBH . I don't know how to react. That was a wierd reaction from my brain)"],
+      12: ["Scientific astrology was going on"],
+      13: ["Venn diagram and stuffs went on"],
+      14: [" The day i said you my purpose 😉😉"],
+      16: ["I sent you a snap about how was my pen flipping skills with my fingers . I sent to you intentionally to build a Convo "]
+      17: ["The day we went to drink some juice and juice was all a shit show where ravi took us and I was eager to take you in my bike but you had other plans🙂"]
+      18: ["Me and fawaaz went out and i was describing you as a fairy to him and you are a perfect woman and stuff and all of a sudden you texted me  venune panrela ne for my zorro snap."]
+      19: ["That night i bought you dosa and waited near sumukha and you were walking inbetween those lights and it seemed like a Angel walking towards me I was mesmerized and mouieas wide open in awe and i have no idea how many mosquitos went inside my mouth that night"]
+      20: ["That was the day you saved me scientist and i saved you player.🫂❤️"]
+      23: ["ANORA day The day you torn my heart into some pieces and I was checking up on you the whole day with live location and texts and at the end of the day you were sleeping like a princess in front of me and I was admiring you so much. And that walk from one building to another building and that chaos with security and you stepped in and sneaked in by saying placement coordinator was fun and lovely. And that night out we had some much fun that day and you were playing with my phone that you'll throw it away was lovely and i was admiring you so so so so so much that day. "]
+      24: ["The fun day with laser tag and stuffs and laser tag we were holding hands for the first time and that was the day you said come to oman and i was like sure I'll 😉!! And the bus ride we 4 sitting together had a lot of meaning ful Convo and 2ndu theya sakthi ah verati adichitom aniku 💀😂."]
+      25: ["The scribble day and i wrote all the hints and stuffs in your tshirt and waited for your response and you were all smiles that day running towards me with your tshirt to make me write something"]
+      27: ["It's sayanora and sorry it's all because of shivani i reacted to you like that ik my sorry isn't enough for what I reacted but seriously you looked so awesome in that red saree with those green bangles and those silver jumkas you literally melted me that day🫠🫠. And we were both literally loved each other's presence without express to each other that day actually that was lovely part ( you watching me dance and watching me speak with the mic and i watched you dance and admiring your beauty and frustrated that I can't express anything) it was a mutual admiration without we both expressing that day"]
+      29: ["The ultimate day were we became one🫂 that word doc and my blah blah speech never had guts to propose to a woman before I did it because not everyone is you and you are really taj reena to my selvabharathi "]
     }
   },
 
@@ -26,15 +26,15 @@ const calendarData = {
     year: 2026,
     month: 2,
     memories: {
-      3: ["☕ Mug & Beans ponom"],
-      4: ["☕ Mug & Beans poitu aprm Kalavasal la pani poori saaptom aprm light balloon, bommai laam vaangunom"],
-      9: ["🌄 Mini Tiffin order potu Samanar Hills la poi saaptom"],
-      10: ["📍 Kalavasal, Miniso, Theppakulam inga laam ponom"],
-      16: ["🧶 Crochet Gift for your Birthday"],
-      18: ["🏍 First time ennoda Bike drive panna"],
-      19: ["☕ Mug & Beans ponom"],
-      20: ["🛵 Scooty Water Wash vitutu aprm Bell Jumbo ponom 🛍 Westside ponom Church ponom aprm yen Bike drive panna"],
-      21: ["SBM aprm ella exams layum pass aaita nu Baskin Robbins la icecream saaptutu Saravana Selvarathinam ponom aprm KFC ponom"],
+      3: ["The day we went for the cricket match and i gave you my uspa cap and it was all yours from that day and i was nice having you in the field but too much evil eyes🧿 on that day iykyk💀"],
+      4: ["The sayalkudi night out and due to someouter tension due to some stupid people i was upset and you showed that text and revealed everything to me and made me feel at peace and we had a lot of beautiful Convo and had our magical first kiss that night at your dad's iconic bolero!!😉"],
+      9: [" The G.O.A.T moment of our story so far kumbalangi night had excellent experience kayaking in the dark and watching the moon and stars with you the best romantic time and morning at thiruparankundram temple giving me the gift it was awesome and at last God gave me my girl at my bday and at a special place . It was all like here is what you wished for and you looked like a Angel who came for me that day 🫠💋❤️. "],
+      10: [" More travel in car and had some time in ramakalmedu and had excellent beef and parotta in pankaj hotel and those ride with you near me was all magical and those dark Misty roads and you near me at night was all totally worth it . It was like a scene straight out of a movie. And had some good dinner in konar kadai parking lot 😉😉❤️."],
+      16: ["It's CDP day and that magical bike ride was awesome who were all hugging me during the entire ride and that Bianca and affogato was sweet and both were top notch but you know that avaniyapuram road was much sweeter😉😉❤️💋!!"],
+      18: ["We played snookers and had a good time in bocs pizza where my fav pic was clicked with both our hands with bracelet and i jumped clg walls after bday bash to meet you and go on a night out with you and that night went so fine with those ride  under the dark sky."],
+      19: ["The night fawaaz left mdu and you were with me consoling me and that day I felt she is really something and I feel better around her, made me realise how important you will be in my life"],
+      20: ["Our date night . Lot of confusion about where to go😂 and went to zorro and had some excellent dosa's in ayyapan dosa kadai. Which was memorable one. Had a long ride in the highways and that hug at TCE road melted your heart ik but i was all eyes about who's coming and going to safeguard you.."],
+      21: [" The day i proposed you with the magical words in kool kadai and it was all magical and totally worth it to say it to you my love❤️🫂. ( Begining of LDR)"],
     }
   },
 
@@ -42,7 +42,7 @@ const calendarData = {
     year: 2026,
     month: 3,
     memories: {
-      7: ["🪽 Siragugal poitu saaptutu un Bday ku gift aa naa hyderabad poi vangitu vantha Lac Bangles kuduthen 🛍 Saravana Selvarathinam aprm Vishaal De Mall aprm Theppakulam ponom anga irunthu un fav dosa saapda famous dosa kadaiku kootitu ponen"],
+      7: ["You sneeked out of your home with venue and we had a good time in ondraga and had a wierd GD in car with everyone looking at us 😂. And roaming in the highway and having some time in the bamboo roadway. It was really a day to remember babes!!"],
     
     }
   },
@@ -51,12 +51,12 @@ const calendarData = {
     year: 2026
     month: 3,
     memories: {
-      5: []
-      6: []
-      7: []
-      8: []
-      9: []
-      15: []
+      5: ["In sree kumaran after opening the screen there was you . You don't know how much relief I had after seeing your smiley face there and i wished it would  be a endless journey to Chennai 🫠"]
+      6: ["Had some quality time in knk road and went to melt and Nutella shake was awesome (ps: i know your choice is always awesome. Iykyk😉😉!) and some time in the beach and it was short day for us."]
+      7: ["The most memorable day and destiny made me give you that ring in that exact Nassau and it was magical and seeing obsession with you made me realise that I was really obsessed with you. And the small time we spent in the beach was really nice and your bodycon outfit was awesome literally melted me and we had some quality time in luxe 🌚And the day ended like a finger snap🫠"]
+      8: ["That morning at mattuthavanai we both don't know that was the last time we are gonna see each other physically for a while 🥺 . It was good until it lasted . Your dad came and picked you up and it was like dust flying and mingling in the air . I was standing there as a stranger and waving you bye 😭🫂"]
+      9: ["The day the official confirmation and the ticket came for your oman departure and I was in tears in the road like a stupid everyone in the road saw me like a fool 🫪!!"]
+      15: ["The day you flew to Oman and my heart shattered into million pieces that day"]
      }
    }
 };
