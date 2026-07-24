72fb000558f123bc80224d2219ad269bd1cc9e8b
TomhollandZendaya-34 <selvabharathi942@gmail.com>
2026-07-24 21:08:40 +0530

    Create calendarData.js

---
+++ calendarData.js
@@ -0,0 +1,62 @@
+const calendarData = {
+  "March": {
+    year: 2026,
+    month: 1,
+    memories: {
+      8: ["📍Tiruppalai la un Frnd Harinee oda Mehandi Function ku unna drop pannen aprm Meenakshi amman temple la irunthu unna pickup pannitu nee church paathadhu illa nu sonna unna church kootitu ponen first time nee church vantha aprm Yum Yards la chicken loaded fries saaptutu clg poitom"],
+      10: ["🏍 Scooty Service eduthutu aprm Mug & Beans poitu clg vanthu scooty ya parking la vitutu clg velila iruntha waffles kadaila waffles saaptom"],
+      11: ["🚶Clg la irunthu Theppakulam vara first time traffic la scooty a drive panna pudhu scooty vaangi first time long journey for you enakum unkooda scooty la vanthathu adhaan first time aprm unaku pudicha 90's mittai theppakulam la vaangunom"],
+      12: ["⛰ Appa Amma Akka kooda barbeque nation porathuku unna thiruparangundram la drop pannitu vanthen"],
+      13: ["🍽 Namma Kadai la night dinner saaptom"],
+      14: ["🌄 Enna First time Samanar Hills kottitu pona anga poitu photo eduthutu aprm gowri sankar hotel la saaptutu trends poitu unaku dress vaangunom"],
+      16: ["🥣 Thirunagar la Mini Idly cravings a fulfill pannom"]
+      17: []
+      18: []
+      19: []
+      20: []
+      23: []
+      24: []
+      25: []
+      27: []
+      29: []
+    }
+  },
+
+  "April": {
+    year: 2026,
+    month: 2,
+    memories: {
+      3: ["☕ Mug & Beans ponom"],
+      4: ["☕ Mug & Beans poitu aprm Kalavasal la pani poori saaptom aprm light balloon, bommai laam vaangunom"],
+      9: ["🌄 Mini Tiffin order potu Samanar Hills la poi saaptom"],
+      10: ["📍 Kalavasal, Miniso, Theppakulam inga laam ponom"],
+      16: ["🧶 Crochet Gift for your Birthday"],
+      18: ["🏍 First time ennoda Bike drive panna"],
+      19: ["☕ Mug & Beans ponom"],
+      20: ["🛵 Scooty Water Wash vitutu aprm Bell Jumbo ponom 🛍 Westside ponom Church ponom aprm yen Bike drive panna"],
+      21: ["SBM aprm ella exams layum pass aaita nu Baskin Robbins la icecream saaptutu Saravana Selvarathinam ponom aprm KFC ponom"],
+    }
+  },
+
+  "May": {
+    year: 2026,
+    month: 3,
+    memories: {
+      7: ["🪽 Siragugal poitu saaptutu un Bday ku gift aa naa hyderabad poi vangitu vantha Lac Bangles kuduthen 🛍 Saravana Selvarathinam aprm Vishaal De Mall aprm Theppakulam ponom anga irunthu un fav dosa saapda famous dosa kadaiku kootitu ponen"],
+    
+    }
+  },
+
+  "June": {
+    year: 2026
+    month: 3,
+    memories: {
+      5: []
+      6: []
+      7: []
+      8: []
+      9: []
+      15: []
+     }
+   }
+};
