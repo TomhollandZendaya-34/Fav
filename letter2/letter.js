df5ed86e456efed1be8e156ebe86b5c9e553e4be
TomhollandZendaya-34 <selvabharathi942@gmail.com>
2026-07-24 21:22:59 +0530

    Add animation control for letter opening feature

---
+++ letter2/letter.js
@@ -0,0 +1,28 @@
+const animation = lottie.loadAnimation({
+    container: document.getElementById("lottie"),
+    renderer: "svg",
+    loop: false,
+    autoplay: false,
+    path: "../assets/envelope.json"
+});
+
+animation.setSpeed(2.5);
+
+const openBtn = document.getElementById("openBtn");
+const lottieBox = document.getElementById("lottie");
+const letter = document.getElementById("letter");
+const continueBtn = document.getElementById("continueBtn");
+
+openBtn.addEventListener("click", () => {
+    openBtn.style.display = "none";
+    animation.goToAndPlay(0, true);
+});
+
+animation.addEventListener("complete", () => {
+    lottieBox.style.display = "none";
+    letter.classList.remove("hidden");
+});
+
+continueBtn.addEventListener("click", () => {
+    window.location.href = "../hidden.html";
+});
